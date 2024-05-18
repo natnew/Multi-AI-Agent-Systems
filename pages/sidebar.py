3eb03869b6f130786bc473f6cd1ab7eb57891f15
Natasha <natashanewboldlondon@gmail.com>
2024-05-18 18:49:32 +0100

    Update sidebar.py

--- pages/sidebar.py
+++ pages/sidebar.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 def add_sidebar():
     with st.sidebar:
+        st.title("Multi-AI-Agent-Systems")
         openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
         st.markdown("[Get an OpenAI API key](https://platform.openai.com/account/api-keys)")
         st.markdown("[View the source code](https://github.com/natnew/Multi-AI-Agent-Systems/blob/main/Chatbot.py)")
