e576f705e509c61011e50d84a87449aa0836c850
Natasha <natashanewboldlondon@gmail.com>
2024-05-18 14:27:10 +0100

    Update crewAI.py

--- pages/crewAI.py
+++ pages/crewAI.py
@@ -1,12 +1,12 @@
 import streamlit as st
+import anthropic
 
 with st.sidebar:
-    OpenAI_api_key = st.text_input("OpenAI API Key", key="file_qa_api_key", type="password")
-    "[View the source code](https://github.com/natnew/Multi-AI-Agent-Systems/blob/main/pages/crewAI.py)"
-   
+    anthropic_api_key = st.text_input("Anthropic API Key", key="file_qa_api_key", type="password")
+    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/1_File_Q%26A.py)"
+    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-st.title("📝 Multi AI Agent Systems with crewAI")
-
+st.title("📝 File Q&A with Anthropic")
 uploaded_file = st.file_uploader("Upload an article", type=("txt", "md"))
 question = st.text_input(
     "Ask something about the article",
@@ -14,9 +14,23 @@ question = st.text_input(
     disabled=not uploaded_file,
 )
 
+if uploaded_file and question and not anthropic_api_key:
+    st.info("Please add your Anthropic API key to continue.")
 
-if uploaded_file and question and not OpenAI_api_key:
-    st.info("Please add your OpenAI API key to continue.")
+if uploaded_file and question and anthropic_api_key:
+    article = uploaded_file.read().decode()
+    prompt = f"""{anthropic.HUMAN_PROMPT} Here's an article:\n\n<article>
+    {article}\n\n</article>\n\n{question}{anthropic.AI_PROMPT}"""
+
+    client = anthropic.Client(api_key=anthropic_api_key)
+    response = client.completions.create(
+        prompt=prompt,
+        stop_sequences=[anthropic.HUMAN_PROMPT],
+        model="claude-v1",  # "claude-2" for Claude 2 model
+        max_tokens_to_sample=100,
+    )
+    st.write("### Answer")
+    st.write(response.completion)
 
 
 
