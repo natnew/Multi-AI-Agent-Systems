8d93c006feacd2c0091f4a81c0abc17aa4c1d1f8
Natasha <natashanewboldlondon@gmail.com>
2024-05-18 18:58:00 +0100

    Update 5_Q&AGPT.py

--- pages/5_Q&AGPT.py
+++ pages/5_Q&AGPT.py
@@ -2,6 +2,13 @@ import streamlit as st
 import openai
 
 with st.sidebar:
+    st.markdown("# About")
+    st.markdown(
+       "A collection of Multi AI Agent Systems "
+            )
+    st.markdown(
+       "This tool is a work in progress. "
+            )
     openai_api_key = st.text_input("OpenAI API Key", key="file_qa_api_key", type="password")
     "[View the source code](https://github.com/natnew/Multi-AI-Agent-Systems/1_CrewAI.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
