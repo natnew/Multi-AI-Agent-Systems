ebb265e0ee1b953ba1612f50d8c2e74f8695dc69
Natasha <natashanewboldlondon@gmail.com>
2024-05-18 18:37:22 +0100

    Update and rename 1_CrewAI.py to 1_CrewAI_Agents.py

--- pages/1_CrewAI_Agents.py
+++ pages/1_CrewAI_Agents.py
@@ -6,7 +6,7 @@ with st.sidebar:
     "[View the source code](https://github.com/natnew/Multi-AI-Agent-Systems/1_CrewAI.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-st.title("💬 Multi AI Agent Systems with crewAI")
+st.title("💬 Multi AI Agent Systems with CrewAI")
 uploaded_file = st.file_uploader("Upload an article", type=("txt", "md"))
 question = st.text_input(
     "Ask something about the article",
